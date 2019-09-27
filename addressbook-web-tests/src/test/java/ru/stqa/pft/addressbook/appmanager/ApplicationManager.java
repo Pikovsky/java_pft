1ea0c8e47b74397a799ccc560dba64dad85e010e
Serge <sergepikovsky@gmail.com>
2019-09-27 17:18:00 +0300

    Закончено разделение класса ApplicationManager. Создан также KatalonHelper.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,16 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.fail;
 
 public class ApplicationManager {
-  WebDriver wd;
+  private WebDriver wd;
+  private KatalonHelper katalonHelper;
+  private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
 
   public void init() {
     System.setProperty("webdriver.gecko.wd", "libs/geckodriver.exe");
@@ -19,57 +19,16 @@ public class ApplicationManager {
     wd.get("http://127.0.0.1:8080/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+    sessionHelper = new SessionHelper(wd);
+    katalonHelper = new KatalonHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
   public void stop() {
     wd.quit();
-    String verificationErrorString = verificationErrors.toString();
+    String verificationErrorString = katalonHelper.verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
       fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = wd.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
     }
   }
 
@@ -81,7 +40,11 @@ public class ApplicationManager {
     return navigationHelper;
   }
 
-  public void gotoGroupPage() {
-    navigationHelper.gotoGroupPage();
+  public SessionHelper getSessionHelper() {
+    return sessionHelper;
+  }
+
+  public KatalonHelper getKatalonHelper() {
+    return katalonHelper;
   }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/KatalonHelper.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.*;
+
+public class KatalonHelper {
+  private WebDriver wd;
+  protected StringBuffer verificationErrors = new StringBuffer();
+  private boolean acceptNextAlert = true;
+
+  public KatalonHelper(WebDriver wd) {
+    this.wd = wd;
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = wd.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
