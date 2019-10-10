87e0478e9e803fb98537cf3a7a57b099e1e02876
Serge <sergepikovsky@gmail.com>
2019-10-10 09:55:27 +0300

    Закончен тест-класс ContactPhoneTests. В тесте заюзан Regex.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -86,11 +86,18 @@ public class ContactHelper extends HelperBase {
     Set<ContactData> contacts = new HashSet<>();
     List<WebElement> rows =  wd.findElements(By.name("entry"));
     for(WebElement row : rows){
-      List<WebElement> cels = row.findElements(By.tagName("td"));
-      int id = Integer.parseInt(cels.get(0).findElement(By.tagName("input")).getAttribute("id"));
-      String lastname = cels.get(1).getText();
-      String firstname = cels.get(2).getText();
-      contacts.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
+      List<WebElement> cells = row.findElements(By.tagName("td"));
+      int id = Integer.parseInt(cells.get(0).findElement(By.tagName("input")).getAttribute("id"));
+      String lastname = cells.get(1).getText();
+      String firstname = cells.get(2).getText();
+      String[] phones = cells.get(5).getText().split("\n");
+      contacts.add(new ContactData()
+              .withId(id)
+              .withFirstname(firstname)
+              .withLastname(lastname)
+              .withHomePhone(phones[0])
+              .withMobilePhone(phones[1])
+              .withWorkPhone(phones[2]));
     }
     return contacts;
   }
