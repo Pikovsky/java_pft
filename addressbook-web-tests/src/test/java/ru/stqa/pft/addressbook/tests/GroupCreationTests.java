d981f2b3eafe48cf4f2f88cdb1c9039b91344cc2
Serge <sergepikovsky@gmail.com>
2019-11-05 20:07:37 +0200

    GroupDataGenerator и GroupCreationTests теперь работают с json форматом.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,7 +18,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() throws IOException {
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
 //    List<Object[]> list = new ArrayList<>();
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
 
@@ -40,7 +41,31 @@ public class GroupCreationTests extends TestBase {
             .iterator();
   }
 
-  @Test(dataProvider = "validGroups")
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+
+    String json = "";
+    String line = reader.readLine();
+    while (line != null){
+      json += line;
+      line = reader.readLine();
+    }
+    //XStream xstream = new XStream();
+    //xstream.processAnnotations(GroupData.class);
+    //List<GroupData> groups = (List<GroupData>) xstream.fromXML(json);
+    Gson gson = new Gson();
+    gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+//  List<GroupData> groups =   gson.fromJson(json, List<GroupData>.class); // к сожалению так указывать можно только "простые" классы.
+    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType()); // это вместо List<GroupData>.class
+    return groups
+            .stream()
+            .map(g -> new Object[]{g})
+            .collect(Collectors.toList())
+            .iterator();
+  }
+
+  @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) throws Exception {
     //GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
     app.goTo().groupPage();
