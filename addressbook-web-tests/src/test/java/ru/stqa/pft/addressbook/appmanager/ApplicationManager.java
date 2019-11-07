5e2521154c186811fbcdbe3f5ee98a69b5293ea5
Serge <sergepikovsky@gmail.com>
2019-11-07 21:49:56 +0200

    Описана конфигурация тестового стенда в resources/local.properties

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.fail;
 
 public class ApplicationManager {
+  private final Properties properties;
   private WebDriver wd;
   protected StringBuffer verificationErrors = new StringBuffer();
   private SessionHelper sessionHelper;
@@ -20,11 +26,13 @@ public class ApplicationManager {
 
   public ApplicationManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
-    // Что бы не создавать Property можно разместить драйвера в папке
-    // прописанной в Path. Многим это нравиться больше чем захламлять ими проект.
+  public void init() throws IOException {
+    String target = System.setProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
     System.setProperty("webdriver.gecko.driver", "libs/geckodriver.exe");
     System.setProperty("webdriver.chrome.driver", "libs/chromedriver.exe");
     System.setProperty("webdriver.ie.driver", "libs/IEDriverServer.exe");
@@ -38,13 +46,15 @@ public class ApplicationManager {
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://127.0.0.1:8080/addressbook/");
+    //wd.get("http://127.0.0.1:8080/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     contactHelper = new ContactHelper(wd);
 
-    sessionHelper.login("admin", "secret");
+    //sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
